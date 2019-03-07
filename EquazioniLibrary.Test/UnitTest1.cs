8d0b6bc8a8911e8c369c07f47dbd695b8d1b555e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-07 08:53:47 +0100

    Add project files.

---
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EquazioniLibrary.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Test per metodo IsDetermined
+        [TestMethod]
+        public void IsDeterminedTest()
+        {
+            //valori passati al metodo da testare
+            double n1 = 0;
+            double n2 = 5;
+            bool ris_atteso = false;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsDetermined(n1, n2);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+        [TestMethod]
+        public void IsDeterminedTest1()
+        {
+            //valori passati al metodo da testare
+            double n1 = 5;
+            double n2 = 0;
+            bool ris_atteso = true;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsDetermined(n1, n2);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+        [TestMethod]
+        public void IsDeterminedTest2()
+        {
+            //valori passati al metodo da testare
+            double n1 = 0;
+            double n2 = 0;
+            bool ris_atteso = false;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsDetermined(n1, n2);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+        [TestMethod]
+        public void IsDeterminedTest3()
+        {
+            //valori passati al metodo da testare
+            double n1 = 5;
+            double n2 = 6;
+            bool ris_atteso = true;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsDetermined(n1, n2);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+
+
+        //Test per metodo IsInconsisted
+        [TestMethod]
+        public void IsInconsistedTest()
+        {
+            //valori passati al metodo da testare
+            double n1 = 0;
+            double n2 = 5;
+            bool ris_atteso = true;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsInconsisted(n1, n2);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+        [TestMethod]
+        public void IsInconsistedTest1()
+        {
+            //valori passati al metodo da testare
+            double n1 = 5;
+            double n2 = 0;
+            bool ris_atteso = false;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsInconsisted(n1, n2);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+        [TestMethod]
+        public void IsInconsistedTest2()
+        {
+            //valori passati al metodo da testare
+            double n1 = 0;
+            double n2 = 0;
+            bool ris_atteso = false;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsInconsisted(n1, n2);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+        [TestMethod]
+        public void IsInconsistedTest3()
+        {
+            //valori passati al metodo da testare
+            double n1 = 5;
+            double n2 = 6;
+            bool ris_atteso = false;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsInconsisted(n1, n2);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+
+
+
+        //Test per metodo IsIndetermined
+        [TestMethod]
+        public void IsIndeterminedTest()
+        {
+            //valori passati al metodo da testare
+            double n1 = 0;
+            double n2 = 5;
+            bool ris_atteso = false;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsIndetermined(n1, n2);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+        [TestMethod]
+        public void IsIndeterminedTest1()
+        {
+            //valori passati al metodo da testare
+            double n1 = 5;
+            double n2 = 0;
+            bool ris_atteso = false;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsIndetermined(n1, n2);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+        [TestMethod]
+        public void IsIndeterminedTest2()
+        {
+            //valori passati al metodo da testare
+            double n1 = 0;
+            double n2 = 0;
+            bool ris_atteso = true;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsIndetermined(n1, n2);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+        [TestMethod]
+        public void IsIndeterminedTest3()
+        {
+            //valori passati al metodo da testare
+            double n1 = 5;
+            double n2 = 6;
+            bool ris_atteso = false;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsIndetermined(n1, n2);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+
+
+
+
+        //Test per metodo IsDegree2
+        [TestMethod]
+        public void IsDegree2Test()
+        {
+            //valori passati al metodo da testare
+            double n1 = 0;
+            double n2 = 5;
+            double n3 = 6;
+            bool ris_atteso = false;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsDegree2(n1, n2, n3);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+        [TestMethod]
+        public void IsDegree2Test1()
+        {
+            //valori passati al metodo da testare
+            double n1 = 5;
+            double n2 = 0;
+            double n3 = 6;
+            bool ris_atteso = true;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsDegree2(n1, n2, n3);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+        [TestMethod]
+        public void IsDegree2Test2()
+        {
+            //valori passati al metodo da testare
+            double n1 = 5;
+            double n2 = 6;
+            double n3 = 0;
+            bool ris_atteso = true;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsDegree2(n1, n2, n3);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+        [TestMethod]
+        public void IsDegree2Test3()
+        {
+            //valori passati al metodo da testare
+            double n1 = 0;
+            double n2 = 0;
+            double n3 = 0;
+            bool ris_atteso = false;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsDegree2(n1, n2, n3);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+
+        [TestMethod]
+        public void IsDegree2Test4()
+        {
+            //valori passati al metodo da testare
+            double n1 = 5;
+            double n2 = 6;
+            double n3 = 7;
+            bool ris_atteso = true;
+
+            //descrivo tutto il percorso che il metodo deve fare per arrivare su ris_calcolato
+            bool ris_calcolato = EquazioniLibrary.Equazioni.IsDegree2(n1, n2, n3);
+            //test che afferma che se ris_atteso e ris_calcolato sono uguali allora va bene
+            Assert.AreEqual(ris_atteso, ris_calcolato);
+        }
+    }
+}
---
+++ EquazioniLibrary/Equazioni.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquazioniLibrary
+{
+    public class Equazioni
+    {
+        //1 Verificare se un'equazione di primo grado è determinata
+        static public bool IsDetermined(double n1, double n2)
+        {
+            bool risp = false;
+
+            if (n1 != 0) 
+            {
+                risp = true;
+            }
+
+            return risp;
+        }
+
+        //1.0 Verificare se un'equazione di primo grado è indeterminata
+        static public bool IsIndetermined(double n1, double n2)
+        {
+            bool risp = false;
+
+            if (n1 == 0 && n2 == 0)
+            {
+                risp = true;
+            }
+
+            return risp;
+        }
+
+        //2 Verificare se un'equazione di primo grado è impossibile
+        static public bool IsInconsisted(double n1, double n2)
+        {
+            bool risp = false;
+
+            if (n1 == 0 && n2 != 0)
+            {
+                risp = true;
+            }
+
+            return risp;
+        }
+
+
+
+
+        //3 Verificare se un'equazione è di secondo grado
+        static public bool IsDegree2(double n1, double n2, double n3)
+        {
+            bool risp = false;
+
+            if (n1 != 0)
+            {
+                risp = true;
+            }
+
+            return risp;
+        }
+
+        //4 Calcolo il Delta
+        static public double Delta(double n1, double n2, double n3)
+        {
+            double delta = 0;
+
+            if (n1 != 0)
+            {
+                delta = (n2 * n2) - (4 * n1 * n3);
+            }
+
+            return delta;
+        }
+
+        //5 Verificare se un'equazione è di secondo grado
+        static public string EquationDegree1(double n1, double n2, double n3)
+        {
+            string risp = "";
+
+            if (Delta(n1, n2, n3) == 0) 
+            {
+                risp = "indeterminata";
+            }
+            else if (Delta(n1, n2, n3) > 0)
+            {
+                risp = "determinata";
+            }
+            else if (Delta(n1, n2, n3) < 0)
+            {
+                risp = "impossibile";
+            }
+
+            return risp;
+        }
+    }
+}
